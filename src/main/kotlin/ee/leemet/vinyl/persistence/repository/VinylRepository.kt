809b7002b827fc012043e6fdfd42df5074ed748a
Sander Leemet <sander.leemet@gmail.com>
2020-12-19 00:49:02 +0200

    cloudbuild

--- src/main/kotlin/ee/leemet/vinyl/persistence/repository/VinylRepository.kt
+++ src/main/kotlin/ee/leemet/vinyl/persistence/repository/VinylRepository.kt
@@ -4,5 +4,11 @@ import ee.leemet.vinyl.model.Vinyl
 
 class VinylRepository {
     fun getVinylWishlist() =
-        listOf(Vinyl("vinüül 1"), Vinyl("Vibyl 2"), Vinyl("uus"), Vinyl("kskdfkskfskfksd"))//TODO
+        listOf(
+            Vinyl("vinüül 1"),
+            Vinyl("Vibyl 2"),
+            Vinyl("uus"),
+            Vinyl("kskdfkskfskfksd"),
+            Vinyl("SEE TÖÖTAB RAISK")
+        )//TODO
 }
