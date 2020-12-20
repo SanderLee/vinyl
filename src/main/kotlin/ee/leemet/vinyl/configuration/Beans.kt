88077e06052755af9ec21b0b304d4a16d169174e
Sander Leemet <sander.leemet@gmail.com>
2020-12-20 18:25:03 +0200

    cors

--- src/main/kotlin/ee/leemet/vinyl/configuration/Beans.kt
+++ src/main/kotlin/ee/leemet/vinyl/configuration/Beans.kt
@@ -7,10 +7,22 @@ import ee.leemet.vinyl.service.VinylService
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.support.GenericApplicationContext
 import org.springframework.context.support.beans
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 fun beans() = beans {
     bean<VinylService>()
     bean<VinylRepository>()
+
+    bean(isPrimary = true) {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("*")
+        config.allowedMethods = listOf("*")
+        config.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/vinyl/**", config)
+        source
+    }
 }
 
 class BeansInitializer : ApplicationContextInitializer<GenericApplicationContext> {
